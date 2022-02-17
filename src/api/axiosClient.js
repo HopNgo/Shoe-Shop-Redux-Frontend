5ee3abd389ace6feb04a13058c5719857e460d24
HopNgo <ngoxuanhop254@gmail.com>
2022-02-17 17:53:35 +0700

    updated

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://shoe-shop-app-hopngo.herokuapp.com/api',
     headers: {
         'content-type': 'application/json',
     },
